b8e35f72a3e7b95b56d6edc484a9594b061e421d
Cody Herbst <codyherbst@Codys-MacBook-Pro.local>
2020-04-01 11:14:58 -0400

    Updates

---
+++ precache-manifest.fe773aed23f60c3d13983b62032ea0a8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "496d7a7c8627d970bf68724c31485c12",
+    "url": "/codyherbst/react-restaurant.git/index.html"
+  },
+  {
+    "revision": "198d403a64ee2a3963d6",
+    "url": "/codyherbst/react-restaurant.git/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "c67b53dacac6bd566d57",
+    "url": "/codyherbst/react-restaurant.git/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "198d403a64ee2a3963d6",
+    "url": "/codyherbst/react-restaurant.git/static/js/2.61cf756c.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/codyherbst/react-restaurant.git/static/js/2.61cf756c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c67b53dacac6bd566d57",
+    "url": "/codyherbst/react-restaurant.git/static/js/main.7f2bd823.chunk.js"
+  },
+  {
+    "revision": "37981e3952216d0aa344",
+    "url": "/codyherbst/react-restaurant.git/static/js/runtime-main.61b09f3b.js"
+  }
+]);
